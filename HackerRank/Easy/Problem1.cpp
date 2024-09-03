f15ffea63c89f81c9e480f281eb6a1a323243ab3
ShamimHossain1 <shamimweb78@gmail.com>
2024-09-03 00:14:45 -0400

    Hack easy Problem1

---
+++ HackerRank/Easy/Problem1.cpp
@@ -0,0 +1,13 @@
+// Output Format
+//  Print Hello, World! to stdout.
+// Sample Output
+//    Hello, World!
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
