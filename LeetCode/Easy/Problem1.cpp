2d8a92821345a53651729c98fa4dc6448915018a
ShamimHossain1 <shamimweb78@gmail.com>
2025-04-04 10:24:06 -0400

    leet prb 1 eas

--- LeetCode/Easy/Problem1.cpp
+++ LeetCode/Easy/Problem1.cpp
@@ -1,10 +1,22 @@
-#include<iostream>
 
-using namespace std;
+#include <iostream>
+#include <vector>
+// Solution 1: (Brute Force)
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            int n = nums.size();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i+1; j < n; j++){
+                    if (nums[i] + nums[j] == target){
+                        return std::vector<int> {i, j};
+                    }                        
+                }
+            }
+            return {};
+            
+        }
+    };
 
-int main(){
 
-    
-
-    return 0;
-}
